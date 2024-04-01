3fe4c184ff3c3c0c41f3164c52ef327db22e675d
Favour <hello.williamsfav@gmail.com>
2024-03-31 21:51:11 -0700

    added firebase and configure it

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:food_app/firebase_options.dart';
 import 'package:food_app/models/restaurants.dart';
 import 'package:food_app/pages/home_page.dart';
 import 'package:food_app/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  //get instance of firebase auth
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  //get current user
+  User? getCurrentUser() {
+    return _firebaseAuth.currentUser;
+  }
+
+  //sign in
+  Future<UserCredential> signInUser(String email, String password) async {
+    try {
+      //sign user in
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
+  //sign up
+  Future<UserCredential> signUpUser(String email, String password) async {
+    try {
+      //sign user in
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
+
+  //sign out
+}
